fb8b55f6f72cbc6b8363e3453c7fed48831aad07
dwi wijonarko <dwi.wijonarko@gmail.com>
2024-12-17 15:56:25 +0700

    update dashboard widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/about_page.dart';
 import 'package:flutter_application_1/pages/gallery_page.dart';
-import 'package:flutter_application_1/pages/setting_page.dart';
 import 'package:flutter_application_1/pages/profile_page.dart';
-import 'package:flutter_application_1/widgets/custom_drawer.dart';
+import 'package:flutter_application_1/pages/setting_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,51 +11,199 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, // Tambahkan baris ini
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Inixindo',
       theme: ThemeData(
-          useMaterial3: false,
-          colorScheme: ColorScheme.fromSwatch(
-            primarySwatch: Colors.blue,
-          ).copyWith(
-            secondary: Colors.blueAccent,
-          )),
-      home: const MyHomePage(title: 'Hello World'),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: false,
+      ),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+enum MenuEnum {
+  beranda,
+  jadwalPerkuliahan,
+  jadwalSkripsi,
+  evaluasiDosen,
+  evaluasiProdi,
+}
+
 class _MyHomePageState extends State<MyHomePage> {
-  void _showSnackBar() {
-    // Show a SnackBar
+  var _submenuEvaluasi = false;
+  var _submenuJadwal = false;
+  var _selectedMenu = MenuEnum.beranda;
+
+  tampilkanSnackBar() {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
-        content: Container(
-          padding: const EdgeInsets.all(8.0),
-          child: Text('Hello from snackbar!'),
+        content: Text('Menu ini belum diimplementasi'),
+        backgroundColor: Colors.blue[200],
+        action: SnackBarAction(
+          label: 'OK',
+          backgroundColor: Colors.white,
+          onPressed: () {
+            tampilkanModalBottomSheet();
+          },
         ),
-        duration: Duration(seconds: 2),
       ),
     );
+  }
+
+  tampilkanAlert() {
+    showDialog(
+        context: context,
+        builder: (ctx) {
+          return AlertDialog(
+            title: Text('Pesan'),
+            content: Text(
+                'Selamat datang di aplikasi Fasilkom App. Gunakan aplikasi ini untuk mendukung produktivitas anda!'),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('OK')),
+            ],
+          );
+        });
+  }
+
+  tampilkanModalBottomSheet() {
+    showModalBottomSheet(
+        context: context,
+        builder: (ctx) {
+          return Container(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            height: 200,
+            child: Column(
+              children: [
+                Text(
+                  'Pilih salah satu opsi!',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          Future.delayed(Duration(milliseconds: 400), () {
+                            Navigator.of(context).pop();
+                            Future.delayed(Duration(milliseconds: 300), () {
+                              tampilkanAlert();
+                            });
+                          });
+                        },
+                        child: Text('Opsi 1'),
+                      ),
+                      TextButton(onPressed: () {}, child: Text('Opsi 2')),
+                      TextButton(onPressed: () {}, child: Text('Opsi 3')),
+                      TextButton(onPressed: () {}, child: Text('Opsi 4')),
+                      TextButton(onPressed: () {}, child: Text('Opsi 5')),
+                      TextButton(onPressed: () {}, child: Text('Opsi 6')),
+                      TextButton(onPressed: () {}, child: Text('Opsi 7')),
+                      TextButton(onPressed: () {}, child: Text('Opsi 8')),
+                      TextButton(onPressed: () {}, child: Text('Opsi 9')),
+                      TextButton(onPressed: () {}, child: Text('Opsi 10')),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: const CustomDrawer(),
-      appBar: AppBar(title: Text(widget.title), actions: [
+      appBar: buildAppBar(context),
+      body: buildBody(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          tampilkanSnackBar();
+        },
+        tooltip: 'Increment',
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        child: const Icon(Icons.add),
+      ),
+      drawer: Builder(builder: (ctx) {
+        return SizedBox(
+          width: MediaQuery.of(context).size.width * 0.8,
+          child: buildDrawer(),
+        );
+      }),
+    );
+  }
+
+  Widget buildBody() {
+    if (_selectedMenu == MenuEnum.jadwalPerkuliahan) {
+      return buildBodyJadwalPerkuliahan();
+    }
+    return buildBodyBeranda();
+  }
+
+  Widget buildBodyJadwalPerkuliahan() {
+    var listMataKuliah = [
+      'Algoritma dan Pemrograman',
+      'Sistem Basis Data',
+      'UI/UX',
+      'Sistem Informasi Geografis',
+    ];
+    return ListView(
+      children: [
+        ...(listMataKuliah.map((mk) {
+          return ListTile(
+            title: Text(mk),
+            leading: Icon(Icons.book_online),
+            onTap: () {},
+          );
+        }).toList()),
+      ],
+    );
+  }
+
+  Widget buildBodyBeranda() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text('Selamat datang di Fasilkom App'),
+        ],
+      ),
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context) {
+    var titleText = "FASILKOM APP";
+    if (_selectedMenu == MenuEnum.evaluasiDosen) {
+      titleText = "Evaluasi Dosen";
+    } else if (_selectedMenu == MenuEnum.evaluasiProdi) {
+      titleText = "Evaluasi Program Studi";
+    } else if (_selectedMenu == MenuEnum.jadwalPerkuliahan) {
+      titleText = "Jadwal Perkuliahan";
+    } else if (_selectedMenu == MenuEnum.jadwalSkripsi) {
+      titleText = "Jadwal Sidang Skripsi";
+    }
+    return AppBar(
+      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      title: Text(titleText),
+      actions: [
         // Icon Search
         IconButton(
           icon: Icon(Icons.search),
@@ -80,6 +227,7 @@ class _MyHomePageState extends State<MyHomePage> {
             );
           },
         ),
+        // Menu PopUp yang sudah ada
         PopupMenuButton<String>(
           onSelected: (String value) {
             switch (value) {
@@ -105,39 +253,144 @@ class _MyHomePageState extends State<MyHomePage> {
           },
           itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
             PopupMenuItem<String>(
-              value: 'Profile', // Menetapkan nilai untuk item menu
+              value: 'Profile',
               child: Text('Profile'),
             ),
             PopupMenuItem<String>(
-              value: 'Gallery', // Menetapkan nilai untuk item menu
+              value: 'Gallery',
               child: Text('Gallery'),
             ),
             PopupMenuItem<String>(
-              value: 'About', // Menetapkan nilai untuk item menu
+              value: 'About',
               child: Text('About'),
             ),
           ],
         ),
-      ]),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Welcome:',
+      ],
+    );
+  }
+
+  Drawer buildDrawer() {
+    return Drawer(
+      child: ListView(
+        children: [
+          UserAccountsDrawerHeader(
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Colors.blue,
+                  Colors.blue.shade900,
+                ],
+              ),
             ),
-            Text(
-              'Mobile Programming',
-              style: Theme.of(context).textTheme.headlineMedium,
+            accountName: Text(
+              'Dwi Wijonarko',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
             ),
-          ],
-        ),
+            accountEmail: Text(
+              'dwi.wijonarko@gmail.com',
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            currentAccountPicture: CircleAvatar(
+              child: Icon(
+                Icons.person,
+                size: 50,
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Beranda'),
+            // trailing: Icon(Icons.chevron_right),
+            onTap: () {
+              Navigator.of(context).pop();
+              setState(() {
+                _selectedMenu = MenuEnum.beranda;
+              });
+            },
+          ),
+          ExpansionTile(
+            title: Text('Jadwal'),
+            leading: Icon(Icons.calendar_month),
+            trailing: _submenuJadwal == false
+                ? Icon(Icons.keyboard_arrow_right)
+                : Icon(Icons.keyboard_arrow_down),
+            onExpansionChanged: (v) {
+              setState(() {
+                _submenuJadwal = v;
+              });
+            },
+            children: [
+              ListTile(
+                leading: Icon(Icons.school),
+                title: Text('Jadwal Perkuliahan'),
+                // trailing: Icon(Icons.chevron_right),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  setState(() {
+                    _selectedMenu = MenuEnum.jadwalPerkuliahan;
+                  });
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.book),
+                title: Text('Jadwal Skripsi'),
+                // trailing: Icon(Icons.chevron_right),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  setState(() {
+                    _selectedMenu = MenuEnum.jadwalSkripsi;
+                  });
+                },
+              ),
+            ],
+          ),
+          ExpansionTile(
+            title: Text('Evaluasi Kinerja'),
+            leading: Icon(Icons.area_chart),
+            trailing: _submenuEvaluasi == false
+                ? Icon(Icons.chevron_right)
+                : Icon(Icons.keyboard_arrow_down),
+            onExpansionChanged: (v) {
+              setState(() {
+                _submenuEvaluasi = v;
+              });
+            },
+            children: [
+              ListTile(
+                leading: Icon(Icons.person_4),
+                title: Text('Evaluasi Dosen'),
+                // trailing: Icon(Icons.chevron_right),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  setState(() {
+                    _selectedMenu = MenuEnum.evaluasiDosen;
+                  });
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.apartment),
+                title: Text('Evaluasi Program Studi'),
+                // trailing: Icon(Icons.chevron_right),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  setState(() {
+                    _selectedMenu = MenuEnum.evaluasiProdi;
+                  });
+                },
+              ),
+            ],
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _showSnackBar,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
